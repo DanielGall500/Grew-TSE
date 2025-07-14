52645c39f487a744e4adfe0434a00403549957b3
Daniel Gallagher <danie@dg.localdomain>
2025-07-14 20:28:47 +0100

    tests include GREW dependencies

--- tests/test_conllu_parser.py
+++ tests/test_conllu_parser.py
@@ -1,11 +1,9 @@
 from treetse.preprocessing.conllu_parser import ConlluParser
 import pytest
 
-
 @pytest.fixture
 def get_test_set_path() -> str:
     return "./tests/datasets/spanish-test-sm.conllu"
-
 
 @pytest.fixture
 def get_test_constraints() -> dict:
@@ -15,9 +13,8 @@ def get_test_constraints() -> dict:
 @pytest.fixture
 def get_parser(get_test_set_path: str, get_test_constraints: dict) -> ConlluParser:
     parser = ConlluParser()
-    parser.parse(get_test_set_path, None, get_test_constraints)
+    parser.parse(get_test_set_path, get_test_constraints, {})
     return parser
-
 
 def test_build_lexical_item_set(
     get_parser: ConlluParser, get_test_set_path: str
@@ -31,7 +28,7 @@ def test_build_lexical_item_set(
 # test dataset just for these tests
 def test_build_masked_dataset(get_parser: ConlluParser, get_test_set_path: str) -> None:
     constraints = {"Mood": "Sub"}
-    results = get_parser._build_masked_dataset(get_test_set_path, None, constraints, "[MASK]")
+    results = get_parser._build_masked_dataset(get_test_set_path, constraints, None, "[MASK]")
     masked_dataset = results["masked"]
     exception_dataset = results["exception"]
     print(masked_dataset.head())
@@ -41,8 +38,24 @@ def test_build_masked_dataset(get_parser: ConlluParser, get_test_set_path: str) 
 
     assert len(masked_dataset) == 10
 
+def test_build_masked_dataset_grew(get_parser: ConlluParser, get_test_set_path: str) -> None:
+    grew_query = """
+        V [upos=VERB, Mood=Sub];
+    """
+    dependency_node = "V"
+    results = get_parser._build_masked_dataset_grew(get_test_set_path, grew_query, dependency_node, "[MASK]")
+    masked_dataset = results["masked"]
+    exception_dataset = results["exception"]
+
+    exception_dataset.to_csv("tests/output/exceptions.csv", index=False)
+    masked_dataset.to_csv("tests/output/masked_dataset.csv", index=False)
+
+    assert masked_dataset.shape[0] == 7
 
 def test_candidate_set(get_parser: ConlluParser) -> None:
-    target_features = {"lemma": "ser", "mood": "Sub", "number": "Sing", "person": "3"}
-    candidates = get_parser.get_candidate_set(target_features)
+    # not including lemma
+    universal_constraints = {"lemma": "ser"}
+    morph_constraints = {"mood": "Sub", "number": "Sing", "person": "3"}
+    candidates = get_parser.get_candidate_set(universal_constraints, morph_constraints)
+    print(candidates)
     assert len(candidates) == 2
---
+++ tests/test_dependencies.py
@@ -0,0 +1,26 @@
+from treetse.preprocessing.grew_dependencies import match_dependencies
+from pathlib import Path
+import pytest
+
+@pytest.fixture
+def get_test_set_path() -> str:
+    base_dir = Path("tests") / "datasets"
+    filename = "spanish-test-sm.conllu"
+    return str(base_dir / filename)
+
+@pytest.fixture
+def get_sample_query() -> str:
+    return """
+    V [upos=VERB];
+    N [upos=NOUN];
+    V -[nsubj]-> N
+    """
+
+def test_match_deps(
+    get_test_set_path: str,
+    get_sample_query: str
+) -> None:
+    dependency_node = "N"
+    deps = match_dependencies(get_test_set_path, get_sample_query, dependency_node)
+    print(deps)
+    assert len(deps) == 2
